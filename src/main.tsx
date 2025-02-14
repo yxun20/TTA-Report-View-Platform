a20820beb869ff77451614c1eb3e4875e0c5d0bd
성영준 <s.yxun20@gmail.com>
2025-02-14 10:21:44 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import MainPage from '@/pages/MainPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+const routesConfig = [
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+];
